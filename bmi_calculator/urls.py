b27a71dc6cd6143d0768ea07917a6cdf4ff5e2ce
Ashis Manna <mannaashis1@gmail.com>
2020-07-26 17:00:55 +0530

    url pattern modification in view

--- bmi_calculator/urls.py
+++ bmi_calculator/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home,name='BMI Calculator'),
+    path('', views.home,name='home'),
      path('calculate_bmi', views.calculate_bmi,name='calculate-bmi'),
 ]
--- bmi_calculator/views.py
+++ bmi_calculator/views.py
@@ -11,7 +11,7 @@ def calculate_bmi(request):
 	if request.POST:
 	    height = float(request.POST.get('height'))
 	    weight = float(request.POST.get('weight'))
-	    return render(request,'bmi_calculator/result.html',{'calculated_bmi' : (weight/height**2) })
+	    return render(request,'bmi_calculator/result.html',{'calculated_bmi' : round((weight/height**2),3) })
 
 def about(request):
 	return render(request,'bmi_calculator/about.html')
